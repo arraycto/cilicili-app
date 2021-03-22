b0f15c8e8598a469bfc8e9ced4d7be8f85e71c65
gaoyuanming <gaoyuanming@gaoyuanmingdeMacBook-Pro.local>
2021-03-22 23:12:09 +0800

    添加视频分辨率适应video标签

--- pages/home/home.js
+++ pages/home/home.js
@@ -6,7 +6,13 @@ Page({
      */
     data: {
         active: 0,
-        roomList: [{}, {}, {}, {}, {}, {}]
+        roomList: [{id: '1'}, {id: '2'}, {id: '3'}, {id: '4'}]
+    },
+
+    clickRoom(e) {
+        wx.navigateTo({
+            url: '/pages/room/room?id=' + e.currentTarget.dataset.id
+        })
     },
 
     /**
@@ -23,45 +29,4 @@ Page({
 
     },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
 })
